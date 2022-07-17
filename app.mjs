be0ba7a53dc9cc5404a8eee3d2e663bbc0771da0
Ope Kareem <kareemope52@gmail.com>
2022-07-17 21:02:17 +0000

    Second-commit

--- app.mjs
+++ app.mjs
@@ -8,7 +8,7 @@ function startApp() {
   
     const header = document.querySelector('h2');
     if(header) {
-      header.textContent = 'Another attempt from a member';
+      header.textContent = 'Second Attempt from a member';
     }
   };
   
