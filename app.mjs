5a7d6e178f2408450f0e9dbff9c02ba420c0ba13
talentlessDeveloper <kareemope52@gmail.com>
2022-07-18 21:41:52 +0100

    attempt from Kareem

--- app.mjs
+++ app.mjs
@@ -1,17 +1,17 @@
 function startApp() {
-    // Your entire app should not necessarily be coded inside this 
-    // single function (though there's no penalty for that), 
-    // so create and use/call additional functions from here
-  
-    // pls remove the below and make some magic in here!
-    console.log('make magic in here!');
-  
-    const header = document.querySelector('h2');
-    if(header) {
-      header.textContent = 'another attempt from Prince';
-    }
-  };
-  
-  // ======= DO NOT EDIT ============== //
-  export default startApp;
-  // ======= EEND DO NOT EDIT ========= //
+  // Your entire app should not necessarily be coded inside this
+  // single function (though there's no penalty for that),
+  // so create and use/call additional functions from here
+
+  // pls remove the below and make some magic in here!
+  console.log("make magic in here!");
+
+  const header = document.querySelector("h2");
+  if (header) {
+    header.textContent = "another attempt from Kareem";
+  }
+}
+
+// ======= DO NOT EDIT ============== //
+export default startApp;
+// ======= EEND DO NOT EDIT ========= //
